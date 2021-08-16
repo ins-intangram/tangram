33027f284e37a5c8e5b75641c9adc427e1f9d2d9
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-08-16 09:24:05 +0200

    nav integration

---
+++ script/script.js
@@ -0,0 +1,14 @@
+var burger = document.querySelector(".button_burger");
+var nav = document.querySelector(".nav");
+
+// open menu
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    nav.classList.toggle("visible");
+})
+
+// close menu
+menu.addEventListener("click", () => {
+    burger.classList.remove("active");
+    nav.classList.remove("visible");
+})
